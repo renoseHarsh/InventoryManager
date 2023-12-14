cde59bae54e9d79e0da64436241712c5193284e6
renoseHarsh <renoseharsh@gmail.com>
2023-12-14 15:32:21 +0530

    did

--- accounts/views.py
+++ accounts/views.py
@@ -87,7 +87,8 @@ def locationInfo(request, location_id):
                'unassigned': options,
                'restock': restock,
                'size': len(options),
-               'isAssigned': request.user.person.location == location}
+               'isAssigned': request.user.person.location 
+               == location}
     
     
     return render(request, 'accounts/locationInfo.html', context)
